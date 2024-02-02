fdc71935b9bd6df74b635946fedae2dd8405700d
sqduan <sqduan@mail.ustc.edu.cn>
2024-02-02 22:06:17 +0800

    Add the everforest scheme into my vim configuration

---
+++ install.sh
@@ -0,0 +1,8 @@
+# This is the installation script to install the essential
+# requirements for my vim configuration, if you don't mind,
+# you can directly use this as your personal vim configuration
+# as well, it based on the everforest color scheme and added
+# some commonly used plugins for development, I hope you enjoy
+# this configuration repo ^-^
+
+
